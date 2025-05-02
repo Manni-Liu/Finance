2040bf3598957657bb357c9ec5a9216c813cef98
Manni Liu <gobomom@163.com>
2025-05-02 21:48:33 +1200

    Add Controllers

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bcd05f8acebe1d7570d7059155dc563f7d5431e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf21e5aebebf3fb12687e9893e1984fdff0e6e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
