f769b8f5dae7dda5afdd4809c0cda15660c5cb8e
Manni Liu <gobomom@163.com>
2025-05-18 21:07:59 +1200

    Add dbContext

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40bfec2514940a304fe736136a56fe0066c050a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2710935289617fad30acba7fcc104f17481e213")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
